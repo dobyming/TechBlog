a4da7ad2e326c1b40efea0028a39720c802f7b58
dobyming <12184874@inha.edu>
2023-04-16 23:45:46 +0900

    Initial Commit - setting gastby and Typescript Settings

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import React, { FunctionComponent } from 'react'
+import Text from 'components/Test'
+
+const IndexPage: FunctionComponent = function () {
+  return <Text text="Just for Testing if Gatsby Framework works well" />
+}
+
+export default IndexPage
