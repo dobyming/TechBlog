d97309e57a064499646669bc4e12fd45eb1c0774
dobyming <12184874@inha.edu>
2023-04-27 16:06:39 +0900

    change github repo name

--- src/components/Post/CommentWidget.tsx
+++ src/components/Post/CommentWidget.tsx
@@ -2,7 +2,7 @@ import React, { createRef, FunctionComponent, useEffect } from 'react'
 import styled from '@emotion/styled'
 
 const src = 'https://utteranc.es/client.js'
-const repo = 'dobyming/TechBlog' // 자신 계정의 레포지토리로 설정
+const repo = 'dobyming/dobyming.github.io' // 자신 계정의 레포지토리로 설정
 
 type UtterancesAttributesType = {
   src: string
