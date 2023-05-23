6f31c66a8ed9d145ec69dd12d9063ed211ee9fdf
dobyming <12184874@inha.edu>
2023-05-24 00:35:47 +0900

    fix: Enhance Validation of SSR Issue

--- src/components/Post/CommentWidget.tsx
+++ src/components/Post/CommentWidget.tsx
@@ -23,55 +23,57 @@ const UtterancesWrapper = styled.div`
 
 const CommentWidget: FunctionComponent = function () {
   const element = createRef<HTMLDivElement>()
-  if (!isBrowser) return null
-  const theme = document.body.classList.contains('dark')
-    ? 'photon-dark'
-    : 'github-light'
+  if (isBrowser()) {
+    const theme = window.document.body.classList.contains('dark')
+      ? 'photon-dark'
+      : 'github-light'
 
-  useEffect(() => {
-    if (element.current === null) return
-    // mount 시 empty script 생성
-    const utterances: HTMLScriptElement = document.createElement('script')
+    useEffect(() => {
+      if (element.current === null) return
+      // mount 시 empty script 생성
+      const utterances: HTMLScriptElement = document.createElement('script')
 
-    const attributes: UtterancesAttributesType = {
-      src,
-      repo,
-      'issue-term': 'pathname',
-      label: 'Comment',
-      theme: theme,
-      crossorigin: 'anonymous',
-      async: 'true',
-    }
+      const attributes: UtterancesAttributesType = {
+        src,
+        repo,
+        'issue-term': 'pathname',
+        label: 'Comment',
+        theme: theme,
+        crossorigin: 'anonymous',
+        async: 'true',
+      }
 
-    Object.entries(attributes).forEach(([key, value]) => {
-      utterances.setAttribute(key, value)
-    })
+      Object.entries(attributes).forEach(([key, value]) => {
+        utterances.setAttribute(key, value)
+      })
 
-    element.current.appendChild(utterances)
-  }, [])
+      element.current.appendChild(utterances)
+    }, [])
 
-  useEffect(() => {
-    const mutationObserver: MutationObserver = new MutationObserver(
-      mutationsList => {
-        mutationsList.forEach(mutation => {
-          if (mutation.attributeName === 'class') {
-            if (document.querySelector('.utterances-frame')) {
-              const theme = mutation.target.classList.contains('dark')
-                ? 'photon-dark'
-                : 'github-light'
-              const message = {
-                type: 'set-theme',
-                theme: theme,
+    useEffect(() => {
+      const mutationObserver: MutationObserver = new MutationObserver(
+        mutationsList => {
+          mutationsList.forEach(mutation => {
+            if (mutation.attributeName === 'class') {
+              if (window.document.querySelector('.utterances-frame')) {
+                const theme = mutation.target.classList.contains('dark')
+                  ? 'photon-dark'
+                  : 'github-light'
+                const message = {
+                  type: 'set-theme',
+                  theme: theme,
+                }
+                const iframe =
+                  window.document.querySelector('.utterances-frame')
+                iframe.contentWindow.postMessage(message, 'https://utteranc.es')
               }
-              const iframe = document.querySelector('.utterances-frame')
-              iframe.contentWindow.postMessage(message, 'https://utteranc.es')
             }
-          }
-        })
-      },
-    )
-    mutationObserver.observe(document.body, { attributes: true })
-  }, [theme])
+          })
+        },
+      )
+      mutationObserver.observe(document.body, { attributes: true })
+    }, [theme])
+  }
 
   return <UtterancesWrapper ref={element} />
 }
